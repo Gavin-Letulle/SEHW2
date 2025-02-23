04c83d3b501798aada7a3b20eee7050b3da3e8f6
Gekire156 <gavin.letulle@icloud.com>
2025-02-23 12:33:27 -0600

    add recipe page

--- routes/addRecipe.js
+++ routes/addRecipe.js
@@ -3,22 +3,44 @@ const router = express.Router();
 const db = require("../database/connection");
 
 router.get('/', (req, res) => {
-    db.query('SELECT * FROM ingredients', (err, ingredients) => {
+    const ingredientsQuery = 'SELECT ingredient_id, ingredient_name FROM ingredients';
+  
+    db.query(ingredientsQuery, (err, ingredients) => {
       if (err) throw err;
       res.render('addRecipe', { title: 'Add New Recipe', ingredients });
     });
   });
   
+  // Route to handle form submission
   router.post('/addRecipe', (req, res) => {
-    const { name, protein_type, ingredient_id, instructions } = req.body;
-    const query = `
-      INSERT INTO recipes (name, protein_type, ingredient_id, instructions)
-      VALUES (?, ?, ?, ?)
-    `;
-    db.query(query, [name, protein_type, ingredient_id, instructions], (err, result) => {
+    const { recipe_name, protein_type, instructions, cooking_time, img_url, ingredient_ids, quantities } = req.body;
+  
+    // Insert into recipes table
+    const insertRecipeQuery = `
+      INSERT INTO recipes (recipe_name, protein_type, instructions, cooking_time, img_url) 
+      VALUES (?, ?, ?, ?, ?)`;
+  
+    db.query(insertRecipeQuery, [recipe_name, protein_type, instructions, cooking_time, img_url || null], (err, result) => {
       if (err) throw err;
-      res.redirect('/recipes');
+  
+      const newRecipeId = result.insertId;
+  
+      // Prepare data for recipe_ingredients table
+      const recipeIngredientsData = ingredient_ids.map((ingredient_id, index) => [
+        newRecipeId,
+        ingredient_id,
+        quantities[index]
+      ]);
+  
+      const insertRecipeIngredientsQuery = `
+        INSERT INTO recipe_ingredients (recipe_id, ingredient_id, quantity) VALUES ?`;
+  
+      db.query(insertRecipeIngredientsQuery, [recipeIngredientsData], (err) => {
+        if (err) throw err;
+  
+        res.redirect(`/recipe/${newRecipeId}`);  // Redirect to the new recipe page
+      });
     });
   });
   
-module.exports = router;
+  module.exports = router;
